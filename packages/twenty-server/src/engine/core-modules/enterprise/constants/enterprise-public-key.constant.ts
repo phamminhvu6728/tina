c027ee0c39bc9e650f4eb09358a932b7230d74a6
nyagami <hoangquan05112002@gmail.com>
2026-08-07 15:44:39 +0700

    fix: update ENTERPRISE_JWT_PUBLIC_KEY

--- packages/twenty-server/src/engine/core-modules/enterprise/constants/enterprise-public-key.constant.ts
+++ packages/twenty-server/src/engine/core-modules/enterprise/constants/enterprise-public-key.constant.ts
@@ -1,14 +1,15 @@
 // RS256 public key for verifying enterprise license JWTs signed by twenty.com
 // The corresponding private key is held exclusively by twenty.com
 export const ENTERPRISE_JWT_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl12Me8NXrQhsgBTr8slx
-2lTZNJCLwWCIs3zRWZzuHelUgNj2wFEM7R7wx0v/OxQHoXzXqAbgEu67HHNXTAnA
-gcYGzjSqa6o8NZHqUrzjOgvP0Ck8EQYxNYrxHAiDUChMHsFNYvcx/savm6Pn1sTL
-gcYnGuuuAYdsV0L78N5WsdbSfyAPyPv5ULYMSci7OLGUBlIa55da9Qwmie1HC+J4
-MtSSnw9o9OzR1ekw9JxQdho+Rj1mQ9BuvBplGNLabolFZweYdYEyqXReRbqMmNz/
-EuZs6PhKiH6l3sXY6kocC0ZV25rFhHgOChVA91BE8a+Wj0MtGGI/UL/b21G3zsr7
-6wIDAQAB
------END PUBLIC KEY-----`;
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApO8V1PZM9k52JEiUOpbA
+zeUjLDG2UnROcTDxFjMdCvw9ZD6JZzaw+B+aV+sn9mEyjRKBeNzLMx/dWZT7PNNZ
+CwW/RTgP20Ir5eoaYr3hSBqw9FuFlDS2wZsE5iDrRDUxp2k1Meq7JOgcd8aGGx8T
+n6MbFUkk42AIch8vJerQ36OzZWwgeq0dwZ37s1izSUBnKvMMaBk8K5FayW9sX9dq
+8KdRz7hy9ELFOuvKALqtZoPqr+BjYRki1Fpvgp/Dh96X9T85B4VZ5jvoYfkfbDnD
+EvrczDTk2KUp/scr4+3ZISycOlYnle4YpRqUrcmxP3ThVnEcH3SndjpCntvoJwbu
+ywIDAQAB
+-----END PUBLIC KEY-----
+`;
 
 // Dev-only
 export const ENTERPRISE_JWT_DEV_PUBLIC_KEY = `-----BEGIN PUBLIC KEY-----
